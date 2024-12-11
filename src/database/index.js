036e4a1e09c2b95272b9b8e2542c14d814fdb489
fikre4231 <fikrebereda@gmail.com>
2024-12-10 18:29:43 -0800

    main

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export default async function connectToDB() {
   try {
     await mongoose.connect(
-      " mongodb+srv://besufekadfikre456:97Xbt07c7AiJaSzX@cluster0.z2i8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+      process.env.MONGO_URL
+    )
     console.log("Database connected successfully");
   } catch (e) {
     console.log(e);
